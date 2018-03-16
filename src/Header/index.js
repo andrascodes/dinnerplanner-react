893eb00ed95e9113a60e5680be693cd53c9f3adf
Andrew Szucs <andrew0szucs@gmail.com>
2018-03-16 16:45:16 +0100

    Add font and image assets and create the Welcome component

---
+++ src/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Header.css'
+import LogoSrc from '../assets/images/logo.png';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <img className="HeaderLogo" src={LogoSrc} alt="Dinnerplanner logo"/>
+      <div className="HeaderTitle">
+        DinnerPlanner
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/Welcome/index.js
+++ src/Welcome/index.js
@@ -3,15 +3,28 @@ import { Link } from 'react-router-dom';
 
 import './Welcome.css';
 
+import RaisedButton from 'material-ui/RaisedButton';
+
+import Header from '../Header'
+
 const Welcome = () => (
   <div className="Welcome">
-    <p>
-        Welcome to the dinner planner React Startup code!
-    </p>
+    <Header />
     
-    <Link to="/search">
-      <button>Start planning</button>
-    </Link>
+    <div className="WelcomeContent">
+      <p>
+        Welcome to the DinnerPlanner!
+      </p>
+      <p>
+        Click the button below to start planning your dinner!
+      </p>
+      
+      <div className="StartButtonContainer">
+        <Link to="/search">
+          <RaisedButton label="Start planning" primary={true} />
+        </Link>
+      </div>
+    </div>
   </div>
 );
 
