7c23f909e0d456ba0b0875ccb40c3cf524381b61
Andrew Szucs <andrew0szucs@gmail.com>
2018-03-21 14:22:46 +0100

    Fix build error caused by leftover exports in views

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,4 @@
-export { default as WelcomeView } from './WelcomeView'
-export { default as DishSearchView } from './DishSearchView'
-export { default as DishDetailsView } from './DishDetailsView'
-export { default as DinnerPlannerView } from './DinnerPlannerView'
-export { default as RecipesView } from './RecipesView'
-export { default as TotalPriceView } from './TotalPriceView'
+export { default as WelcomeView } from './WelcomeView';
+export { default as DinnerPlannerView } from './DinnerPlannerView';
+export { default as RecipesView } from './RecipesView';
+export { default as TotalPriceView } from './TotalPriceView';
