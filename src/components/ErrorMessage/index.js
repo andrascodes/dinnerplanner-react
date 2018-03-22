d81d011a64b3abdb07e7a4522095dce6e0c495ce
Andrew Szucs <andrew0szucs@gmail.com>
2018-03-22 15:16:39 +0100

    Add ErrorMessage component with some style

--- src/components/ErrorMessage/index.js
+++ src/components/ErrorMessage/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import './ErrorMessage.css';
+
+import ErrorIcon from 'material-ui/svg-icons/alert/error';
+import { red500 } from 'material-ui/styles/colors';
+
 const ErrorMessage = ({ message }) => {
   return (
     <div className="ErrorMessage">
-      {message}
+      <div className="ErrorMessageContent">
+        <ErrorIcon 
+          style={{ height: "12rem", width: "12rem"}}
+          color={red500}
+        />
+        <p>Something went wrong!</p>
+        <div className="ErrorMessageText">
+          {`Error message: ${message}`}
+        </div>
+      </div>
     </div>
   );
 };
